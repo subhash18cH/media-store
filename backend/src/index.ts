f28cb6324c3369667ea5a00276872f90637d0a2b
subhash18cH <subhashcnd15@gmail.com>
2025-10-04 22:03:00 +0530

    initialized project

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+
+})
